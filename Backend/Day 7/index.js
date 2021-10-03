b90400de50c35c6be08e65e465c257950bdfb4ab
Mayank Singh Rathore <mayanksinghrathore818@gmail.com>
2021-10-03 19:55:47 +0530

    Day 7
    Day 7

--- Backend/Day 7/index.js
+++ Backend/Day 7/index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send("Mayank");
+});
+
+app.get("/ab+cd", (req, res) => {
+  res.send("abcd");
+  console.log(req.query);
+});
+
+app.listen(5000);
