dbd23e86afcfcf15ed8829413a2cfa5b8b390bba
Mayank Singh Rathore <mayanksinghrathore818@gmail.com>
2021-10-03 22:00:55 +0530

    Day 9
    Day 9

--- Backend/Day 9/index.js
+++ Backend/Day 9/index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/hello.txt"), "text.txt");
+
+  // res
+  //   .status(201)
+  //   .cookie("token", "test", {
+  //     expires: new Date(Date.now() + 4 * 3600000),
+  //   })
+  //   .status(301)
+  //   .cookie("hello", "yesss!")
+  //   .redirect(301, "/admin");
+});
+
+app.listen(5000);
